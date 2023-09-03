3903a77ee1d71b21d233fcac02ec712e8692a50b
MariaJoseDiazCa <mariajose.diazca@gmail.com>
2023-09-03 17:35:09 -0300

    UPDATE: Adding other custom properties files for the texts

---
+++ src/main/java/com/mjdiazc/springboot/web/app/TextosPropertiesConfig.java
@@ -0,0 +1,13 @@
+package com.mjdiazc.springboot.web.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:textos.properties")
+})
+public class TextosPropertiesConfig {
+
+}
